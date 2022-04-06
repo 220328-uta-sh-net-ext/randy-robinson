68f25602fe89666e8cbb710ffe18899ce6ffe78d
havelpot <quiverisdrawn@gmail.com>
2022-04-06 17:25:34 -0400

    Changes to Files
    Also added code for my mathmatics project

---
+++ B/Mathmatics/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Console.WriteLine("Hello, would you like to say something?");
+var bullStuff= Console.Readline();
+Console.Write("Please enter a number ");
+var x=Convert.ToDouble(Console.ReadLine());
+Console.Write("Please enter another number ");
+var y=Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Please enter an operand to calculate the sum: ");
+var z= Console.ReadLine();
+
---
+++ B/Mathmatics/allOperands.cs
@@ -0,0 +1,24 @@
+namespace Mathmatics
+{
+    public class allOperands
+    {
+        public static double adding(double a, double b){
+            return a + b;
+        } 
+        public static double subtract( double a, double b){
+            return a - b;
+        }
+        public static double multiply(double a, double b){
+            return a * b;
+        } 
+        public static double divide( double a, double b){
+            return a / b;
+        }
+        // More logic is needed to execute the modulas command
+        /***public static double modulas(double a, double b){
+            return ;
+        } ***/  
+           
+    }
+
+}
