c9161265cd075e9eab9c18f99e4d793324af8b9b
havelpot <quiverisdrawn@gmail.com>
2022-04-14 10:43:18 -0400

    Created Bank UI; in Solution Bank.

--- Bank/Bank/Program.cs
+++ Bank/Bank/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Bank;
+
+var account = new BankAccount("Jimmy T.", 1000);
+Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+Console.WriteLine(account.Balance);
+account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+Console.WriteLine(account.Balance);
