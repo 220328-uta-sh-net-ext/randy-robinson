03e019e1d0de86af709d07ab8850ab30da861d35
havelpot <quiverisdrawn@gmail.com>
2022-05-03 09:42:12 -0400

    Update to Tables

--- TrainingPractice/Restaurant.sql
+++ TrainingPractice/Restaurant.sql
@@ -10,3 +10,12 @@ CREATE TABLE Restaurant.RestaurantID(
     RestaurantReview NVARCHAR(5),
 
 )
+CREATE TABLE Restaurant.Users(
+            UserTagNumber NVARCHAR(5),
+            UserName NVARCHAR(5),
+            FirstName NVARCHAR(5), 
+            LastName NVARCHAR(5),
+            Password NVARCHAR(5)
+)
+SELECT*
+FROM Restaurant.Users
