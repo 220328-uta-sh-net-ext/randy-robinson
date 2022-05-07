feddb52bedb74fe1fa3562368c970befa8a6d8f2
havelpot <quiverisdrawn@gmail.com>
2022-05-07 11:04:16 -0400

    update to IDisplayMenu

--- Project0/Restraunt/RestrauntUI/IDisplayMenu.cs
+++ Project0/Restraunt/RestrauntUI/IDisplayMenu.cs
@@ -8,5 +8,16 @@ namespace RestUI
 {
     public interface IDisplayMenu
     {
+        /// <summary>
+        /// Will display the menu and user choices in the terminal
+        /// </summary>
+        void Display();
+
+        /// <summary>
+        /// Will record the user's choice and change/route your menu based on that choice
+        /// </summary>
+        /// <returns>Return the menu that will change your screen</returns>
+        string UserChoice();
     }
+
 }
