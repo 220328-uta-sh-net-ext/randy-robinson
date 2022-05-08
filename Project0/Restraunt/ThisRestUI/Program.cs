eee7c7a1b1c55d86bf55a8170d2458bce1574938
havelpot <quiverisdrawn@gmail.com>
2022-05-08 18:21:28 -0500

    One more time

--- Project0/Restraunt/ThisRestUI/Program.cs
+++ Project0/Restraunt/ThisRestUI/Program.cs
@@ -15,9 +15,13 @@
  * 
  **/
 global using Serilog;
+using System.Data.SqlClient;
 using System;
 using ThisRestUI;
 using RestaurantModels;
+using UserModel;
+using ReviewModels;
+using ThisRestDL;
 
 
 
@@ -26,6 +30,13 @@ Log.Logger = new LoggerConfiguration()
     .WriteTo.Console().MinimumLevel.Debug()
     .WriteTo.File("./Logs/user.txt").MinimumLevel.Debug().MinimumLevel.Information()// we want to save the ;ogs in this file
     .CreateLogger();
+string connectionStringFilePath = "../../../../PokemonDL/connection-string.txt";
+string connectionString = File.ReadAllText(connectionStringFilePath);
+
+IRestaurantRepo repository = new SqlRepository(connectionString);
+IRestaurantLogic logic = new RestaurantLogic(repository);
+RestaurantOps operations = new(repository);
+
 Restaurant imNewRestraunt = new Restaurant();
 imNewRestraunt.RestaurantName = "nextTacoTruck";
 DisplayMenu displayMenu = new DisplayMenu();
