067d7806fe68bf465820623d60c51b7007989594
havelpot <quiverisdrawn@gmail.com>
2022-05-09 01:05:45 -0500

    Added Review

--- Project0/Restraunt/ReviewModels/Review.cs
+++ Project0/Restraunt/ReviewModels/Review.cs
@@ -2,6 +2,33 @@
 {
     public class Review
     {
+        public DateTime Date { get; }
+        public string Note { get; set; }
+        public decimal Rating { get; set; }
+        public string UserName { get; set; }
+        public string RestaurantName { get; set; }
         
+        public int ReviewID=0;
+        public Review()
+        {
+            UserName = "-";
+            RestaurantName = "-";
+            Rating = 0;
+            Note = "-";
+            ReviewID++;
+        }
+        public Review(decimal rating, string note) 
+        {
+            UserName = this.UserName;
+            RestaurantName = this.RestaurantName;
+            Rating= rating;
+            Note= note;
+            ReviewID++;
+        }
+        public override string ToString()
+        {
+            string result = $"User Name: {UserName}\nRestaurantName: {RestaurantName}\nNote:{Note}";
+            return result;
+        }
     }
 }
