a120bec82c059c714351b4356fb6103e2e9a5f61
havelpot <quiverisdrawn@gmail.com>
2022-05-09 08:26:27 -0500

    udated Tables

--- Project0/Restraunt/ThisRestDL/Restaurant.sql
+++ Project0/Restraunt/ThisRestDL/Restaurant.sql
@@ -1,1 +1,31 @@
 
+/**-----------------------------------------------------------------------
+                            Create Tables
+-----------------------------------------------------------------------**/
+
+CREATE TABLE [dbo].[Users]
+(
+    [UserTagNumber] INT NULL,
+    [Username] NVARCHAR(100) NOT NULL PRIMARY KEY,
+    [FirstName] NVARCHAR(40) NULL,
+    [LastName] NVARCHAR(50) NULL,
+    [Password] NVARCHAR(20) NULL
+);
+GO
+CREATE TABLE [dbo].[Restaurants]
+(
+   [RestaurantIdseed] INT NOT NULL PRIMARY KEY,
+   [RestaurantName] NVARCHAR(50) NULL,
+   [RestaurantCity] NVARCHAR(50) NULL,
+   [RestaurantState] NVARCHAR(5) NULL,
+   [RestaurantZip] INT NULL,
+   [RestaurantAvgRating] INT NULL
+);
+GO
+CREATE TABLE [dbo].[Reviews]
+(
+   [RestaurantName] NVARCHAR(50) NOT NULL PRIMARY KEY,
+   [Username] NVARCHAR(100) NULL,
+   [Rating] INT NULL,
+   [Note] NVARCHAR(200) NULL 
+);
