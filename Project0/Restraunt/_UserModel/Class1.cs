a0a077729a1cb24fc32610295dc235a2165e003f
havelpot <quiverisdrawn@gmail.com>
2022-05-09 00:04:03 -0500

    another change before bed

---
+++ Project0/Restraunt/_UserModel/Class1.cs
@@ -0,0 +1,36 @@
+﻿namespace _UserModel
+{
+    public class CreateUser
+    {
+
+        //Properties of this class.
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        private bool IsAdmin { get; set; }
+        public int UserTagNumber = 3332979;
+        public int AdminTag = 3332900;
+        enum UserEnum { Admin = 0, RegUser = 1, }
+        public CreateUser()
+        {
+            UserTagNumber++;
+            UserName = " Havelpot ";
+            FirstName = "-";
+            LastName = "-";
+            Password = "-";
+
+        }
+        public CreateUser(string firstName, string lastName, string userName)
+        {
+            UserTagNumber = UserTagNumber++;
+            UserName = userName;
+            FirstName = firstName;
+            LastName = lastName;
+
+        }
+
+        class AdminCreateUser : CreateUser { }
+    }
+
+}
