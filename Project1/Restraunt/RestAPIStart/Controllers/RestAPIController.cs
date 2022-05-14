e418bb5a061a5a1c1d63268f681206052b92944e
havelpot <quiverisdrawn@gmail.com>
2022-05-14 18:22:17 -0500

    Added put and delete available commands on swagger and postman

--- Project1/Restraunt/RestAPIStart/Controllers/RestAPIController.cs
+++ Project1/Restraunt/RestAPIStart/Controllers/RestAPIController.cs
@@ -31,7 +31,11 @@ namespace RestAPIStart.Controllers
             new Restaurant{RestaurantName="thisTapasPlate", RestaurantCity="Macomb", RestaurantState= "IL", RestaurantZip= 64115},
             new Restaurant{RestaurantName="papaNoJohns", RestaurantCity="Sanfrancisco", RestaurantState="CA", RestaurantZip= 34578}
         };
-        // Need a way to create Web Tabs to manipulate resources?? use this:
+        /// <summary>
+        ///  Need a way to create Web Tabs to manipulate resources?? use these:
+        /// </summary>
+        /// </returns> This is the Get operator </returns>
+
         [Authorize]
         [HttpGet] //This is the Get operator
         [ProducesResponseType(200, Type = typeof(List<Restaurant>))]
@@ -51,5 +55,58 @@ namespace RestAPIStart.Controllers
             return Ok(restaurants);
         }
 
-     }
+        [HttpGet("name")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<Restaurant> Get(string name)
+        {
+            var rest= createRestaurant.SearchRestaurant(name);
+            if (rest.Count <= 0)
+                return NotFound($"The Restaurant {name} is currently not in the Database");
+            return Ok(rest);
+        }
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult Post([FromBody] Restaurant rest)
+        {
+            if (rest == null)
+                return BadRequest("Invalid Restaurant, please add valid values");
+            createRestaurant.AddRestaurant(rest);
+            return CreatedAtAction("Get", rest);
+        }
+        [HttpPut]
+        public ActionResult Put([FromQuery]Restaurant rest, [FromBody]string name)
+        {
+            if(name == null)
+                return BadRequest("Not able to modify the Review of a Restaurant without the restaurant name.");
+            try
+            {
+                var restaurant = restaurants.Find(y => y.RestaurantName.Contains(name));
+                if (restaurant == null)
+                    return NotFound("Restaurant Not Found");
+                rest.RestaurantName = rest.RestaurantName.Trim();
+                rest.RestaurantAvgRating = rest.RestaurantAvgRating;
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            return Created("Get", rest);
+        }
+
+        [HttpDelete]
+        public ActionResult Delete(string name)
+        {
+            if (name == null)
+                return BadRequest("Unable to remove Restaurant without the Restaurant Name");
+            var restaurant = restaurants.Find(y => y.RestaurantName.Contains(name));
+            if (restaurant == null)
+                return NotFound("Restaurant not found in database");
+            restaurants.Remove(restaurant);
+            return Ok($"Restaurant {name} Deleted from database");
+        }
+    
+    }
+
 }
