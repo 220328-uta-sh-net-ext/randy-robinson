eadccb9b9dbcb423e874af897a3012ae8d1c155a
havelpot <quiverisdrawn@gmail.com>
2022-06-05 15:10:20 -0500

    update to Angular version_Okta

---
+++ TrainingPractice/Okta/Angular-okta/src/app/app-routing.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { OktaCallbackComponent } from '@okta/okta-angular';
+import { OktaAuthModule } from '@okta/okta-angular';
+const routes: Routes = [{ path: 'login/callback', component: OktaCallbackComponent }];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
