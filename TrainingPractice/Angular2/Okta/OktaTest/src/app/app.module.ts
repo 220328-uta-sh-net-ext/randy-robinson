0a11a3ef2e9df29854abd60a19bff57d26372e86
havelpot <quiverisdrawn@gmail.com>
2022-06-05 18:31:24 -0500

    More updates to Okta

--- TrainingPractice/Angular2/Okta/OktaTest/src/app/app.module.ts
+++ TrainingPractice/Angular2/Okta/OktaTest/src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { OktaAuthModule, OKTA_CONFIG } from '@okta/okta-angular';
+import { OktaAuth } from '@okta/okta-auth-js';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'; 
+
+const oktaAuth = new OktaAuth({
+  issuer: 'https://dev-77390416.okta.com/oauth2/default',
+  clientId: '0oa5aevt2ppy3b26c5d7',
+  redirectUri: window.location.origin + '/login/callback'
+});
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent, 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    OktaAuthModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [{ provide: OKTA_CONFIG, useValue: { oktaAuth } }],
+  bootstrap: [AppComponent],
+  
 })
 export class AppModule { }
