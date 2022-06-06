260a40ca977382fa0ccdd1efa383ecd39c3270b9
havelpot <quiverisdrawn@gmail.com>
2022-06-05 19:41:20 -0500

    Last one you can select the sign in button

--- TrainingPractice/Angular2/Okta/OktaTest/src/app/app.module.ts
+++ TrainingPractice/Angular2/Okta/OktaTest/src/app/app.module.ts
@@ -5,11 +5,10 @@ import { OktaAuth } from '@okta/okta-auth-js';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'; 
 
 const oktaAuth = new OktaAuth({
   issuer: 'https://dev-77390416.okta.com/oauth2/default',
-  clientId: '0oa5aevt2ppy3b26c5d7',
+  clientId: '0oa5afwmcm1PkMWX75d7',
   redirectUri: window.location.origin + '/login/callback'
 });
 
